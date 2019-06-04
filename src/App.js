592da6d262aee19a396d6f8386597c5452b21b28
chrisdennett <chrisdennett@gmail.com>
2019-06-04 15:26:11 +0100

    Added the service worker for offline support.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import "@material/button/dist/mdc.button.css";
 import { Button } from "@rmwc/button";
 // styles
 import "./app.css";
-// comps
-import SocialMediaLinks from "./comps/SocialMediaLinks";
 // data
 import { projects } from "./PROJECTS";
-import Link from "./comps/Link";
-import Ruler from "./comps/Ruler";
+// comps
+import ErrorBoundary from "./comps/ErrorBoundary";
+const SocialMediaLinks = lazy(() => import("./comps/SocialMediaLinks"));
+const Link = lazy(() => import("./comps/Link"));
+const Ruler = lazy(() => import("./comps/Ruler"));
 const ProjectCard = lazy(() => import("./comps/ProjectCard"));
 
 const renderLoader = () => <p>Loading</p>;
@@ -32,122 +33,138 @@ const App = () => {
           </p>
         </div>
         <div className={"contactLinks"}>
-          <SocialMediaLinks className={"contactLinks--socialMediaLinks"} />
-          <Link
-            className={"header-email-link"}
-            text={"chrisdennett@gmail.com"}
-            url={"mailto:chrisdennett@gmail.com"}
-          />
+          <Suspense fallback={renderLoader()}>
+            <SocialMediaLinks className={"contactLinks--socialMediaLinks"} />
+            <Link
+              className={"header-email-link"}
+              text={"chrisdennett@gmail.com"}
+              url={"mailto:chrisdennett@gmail.com"}
+            />
+          </Suspense>
         </div>
       </header>
 
-      <Ruler />
-
-      {/* RECENT PROJECTS */}
-      <section className={"content-section"}>
-        <div className={"sectionHeader"}>
-          <h2 className={"sectionTitle"}>Recent Projects</h2>
-          <p className={"sectionSubtitle"}>
-            The things I make inbetween freelance work.
-          </p>
-        </div>
+      <ErrorBoundary>
         <Suspense fallback={renderLoader()}>
-          <div className={"currentProjects"}>
-            {projects.map(project => {
-              return <ProjectCard key={project.url} {...project} />;
-            })}
-          </div>
+          <Ruler />
         </Suspense>
-      </section>
 
-      <Ruler />
+        {/* RECENT PROJECTS */}
+        <section className={"content-section"}>
+          <div className={"sectionHeader"}>
+            <h2 className={"sectionTitle"}>Recent Projects</h2>
+            <p className={"sectionSubtitle"}>
+              The things I make inbetween freelance work.
+            </p>
+          </div>
+          <Suspense fallback={renderLoader()}>
+            <div className={"currentProjects"}>
+              {projects.map(project => {
+                return <ProjectCard key={project.url} {...project} />;
+              })}
+            </div>
+          </Suspense>
+        </section>
 
-      {/* ARCHIVE */}
-      <section className="content-section otherThings">
-        <div className={"sectionHeader"}>
-          <h2 className={"sectionTitle"}>Quick things</h2>
-          <p className={"sectionSubtitle"}>
-            I'd love to be able to tell you there's a good reason for this
-            section, but really it's just projects I've not got round to taking
-            a screen shot for!
-          </p>
-        </div>
-        <ul className={"otherThings--list"}>
-          <li>
-            <Button
-              style={{ background: "steelblue" }}
-              raised
-              tag={"a"}
-              href="https://lab.artfly.io/portrait-grids"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Portrait Grid Maker
-            </Button>{" "}
-            Something I made for the ArtFly Lab to help people draw portraits
-            using a grid.
-          </li>
-          <li>
-            <Button
-              style={{ background: "steelblue" }}
-              raised
-              tag={"a"}
-              href="https://lab.artfly.io/pixel-stretcher"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Pixel Stretcher
-            </Button>{" "}
-            Stretch out an image like chewing gum.
-          </li>
-          <li>
-            <Button
-              style={{ background: "steelblue" }}
-              raised
-              tag={"a"}
-              href="https://lab.artfly.io/colour-splitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Colour Splitter
-            </Button>{" "}
-            An ArtFly Lab experiment to split the colours in a photo.
-          </li>
-          <li>
-            <Button
-              style={{ background: "steelblue" }}
-              raised
-              tag={"a"}
-              href="https://ticklypictures.com/projects/jellycam/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              JellyCam
-            </Button>{" "}
-            A bit of stop-motion software I made a long time ago.
-          </li>
-        </ul>
-      </section>
+        <Suspense fallback={renderLoader()}>
+          <Ruler />
+        </Suspense>
 
-      <Ruler />
+        {/* ARCHIVE */}
+        <section className="content-section otherThings">
+          <div className={"sectionHeader"}>
+            <h2 className={"sectionTitle"}>Quick things</h2>
+            <p className={"sectionSubtitle"}>
+              I'd love to be able to tell you there's a good reason for this
+              section, but really it's just projects I've not got round to
+              taking a screen shot for!
+            </p>
+          </div>
+          <ul className={"otherThings--list"}>
+            <li>
+              <Button
+                style={{ background: "steelblue" }}
+                raised
+                tag={"a"}
+                href="https://lab.artfly.io/portrait-grids"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Portrait Grid Maker
+              </Button>{" "}
+              Something I made for the ArtFly Lab to help people draw portraits
+              using a grid.
+            </li>
+            <li>
+              <Button
+                style={{ background: "steelblue" }}
+                raised
+                tag={"a"}
+                href="https://lab.artfly.io/pixel-stretcher"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Pixel Stretcher
+              </Button>{" "}
+              Stretch out an image like chewing gum.
+            </li>
+            <li>
+              <Button
+                style={{ background: "steelblue" }}
+                raised
+                tag={"a"}
+                href="https://lab.artfly.io/colour-splitter"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colour Splitter
+              </Button>{" "}
+              An ArtFly Lab experiment to split the colours in a photo.
+            </li>
+            <li>
+              <Button
+                style={{ background: "steelblue" }}
+                raised
+                tag={"a"}
+                href="https://ticklypictures.com/projects/jellycam/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                JellyCam
+              </Button>{" "}
+              A bit of stop-motion software I made a long time ago.
+            </li>
+          </ul>
+        </section>
 
-      {/* FOOTER */}
-      <footer className={"footer"}>
-        <div className={"footer-content"}>
-          <img src={"Rotoscoping.gif"} alt={"rotoscoping gif"} />
+        <Suspense fallback={renderLoader()}>
+          <Ruler />
+        </Suspense>
 
-          <SocialMediaLinks className={"footer--links"} />
-          <Link
-            className={"footer-email-link"}
-            text={"chrisdennett@gmail.com"}
-            url={"mailto:chrisdennett@gmail.com"}
-          />
+        {/* FOOTER */}
+        <footer className={"footer"}>
+          <Suspense fallback={renderLoader()}>
+            <div className={"footer-content"}>
+              {/* <img src={"Rotoscoping.gif"} alt={"rotoscoping gif"} /> */}
+              <img
+                src={"img/rotoscoping.webp"}
+                alt={"rotoscoping victorian lady"}
+              />
 
-          <p className={"footer-content--text"}>
-            You know it's all over when the victorian lady jumps the stool.
-          </p>
-        </div>
-      </footer>
+              <SocialMediaLinks className={"footer--links"} />
+              <Link
+                className={"footer-email-link"}
+                text={"chrisdennett@gmail.com"}
+                url={"mailto:chrisdennett@gmail.com"}
+              />
+
+              <p className={"footer-content--text"}>
+                You know it's all over when the victorian lady jumps the stool.
+              </p>
+            </div>
+          </Suspense>
+        </footer>
+      </ErrorBoundary>
     </div>
   );
 };
